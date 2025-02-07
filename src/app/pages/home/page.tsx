1787c5455f08d208fe839ce56e930b856c1a199d
mlammon11 <mlammon11@gmail.com>
2025-02-07 17:46:21 -0500

    basic calendar setup
    setting up a basic calendar

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,12 +1,48 @@
-
+import React from "react";
 import Navbar from "../../components/navbar";
+import styles from "../../styles/home.module.css";
 
 export default function Home() {
-  
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className={styles.calendar}>
+        <div className={styles.header}>
+          <div className={styles.empty}></div> {/* Empty cell for the top left corner */}
+          <div className={styles.day}>Sunday</div>
+          <div className={styles.day}>Monday</div>
+          <div className={styles.day}>Tuesday</div>
+          <div className={styles.day}>Wednesday</div>
+          <div className={styles.day}>Thursday</div>
+          <div className={styles.day}>Friday</div>
+          <div className={styles.day}>Saturday</div>
+        </div>
+
+        <div className={styles.grid}>
+          {[...Array(24)].map((_, index) => {
+            const hour = 
+            index === 0
+              ? "12 AM" // 12 AM for midnight
+              : index < 12
+              ? `${index} AM` // 1 AM to 11 AM
+              : index === 12
+              ? "12 PM" // 12 PM for noon
+              : `${index - 12} PM`; // 1 PM to 11 PM
+            return (
+              <React.Fragment key={hour}>
+                <div className={styles.hour}>{hour}</div>
+                {[...Array(7)].map((_, dayIndex) => (
+                  <div
+                    key={`${hour}-${dayIndex}`}
+                    className={styles.cell}
+                  ></div>
+                ))}
+              </React.Fragment>
+            );
+          })}
+        </div>
+
+      </div>
     </>
   );
 }
